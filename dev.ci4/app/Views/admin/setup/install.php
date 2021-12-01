e10b6a89df94542522529f225866b167c0634d35
Richard Keasley <rk@base-camp.org.uk>
2021-12-01 13:57:02 +0000

    Updated installation instructions
    Removed unnecessary constants
    Updated path names for clarity

--- dev.ci4/app/Views/admin/setup/install.php
+++ dev.ci4/app/Views/admin/setup/install.php
@@ -1,67 +1,92 @@
 <?php $this->extend('default');
 
+$consts = [
+	'ROOTPATH',
+	'APPPATH',
+	'SYSTEMPATH',
+	'WRITEPATH',
+	'FCPATH'
+];
+// create HTML for path constants
+foreach($consts as $const) {
+	$paths[$const] = path_label($const);
+} 
+
 $this->section('sidebar'); ?>
 <ul class="list-unstyled">
-<li class="bi bi-folder"> [root]<ul class="list-unstyled ps-3">
-	<li class="bi bi-folder"> gymevent.ci4<ul class="list-unstyled ps-3">
-		<li class="bi bi-folder"> app<ul class="list-unstyled ps-3">
+<li class="bi bi-folder"> <?php echo $paths['ROOTPATH'];?>
+<ul class="list-unstyled ps-3">
+	<li class="bi bi-folder"> <?php echo $paths['APPPATH'];?>
+		<ul class="list-unstyled ps-3">
 			<li class="bi bi-list"> [application folders]</li>
 			<li class="bi bi-file-code"> .htaccess</li>
 			<li class="bi bi-file-code"> Common.php</li>
-		</ul></li>
-		<li class="bi bi-folder"> system</li>
-		<li class="bi bi-folder"> writable</li>
-		</ul></li>
-		<li class="bi bi-file-code"> .env</li>
-	</ul></li>
-	<li class="bi bi-folder"> public_html<ul class="list-unstyled ps-3">
-		<li class="bi bi-folder"> public<ul class="list-unstyled ps-3">
-			<li class="bi bi-folder"> ui</li>
-			<li class="bi bi-list"> [site downloads / etc]</li>
-		</ul></li>
+		</ul>
+	</li>
+	<li class="bi bi-folder"> <?php echo $paths['SYSTEMPATH'];?></li>
+	<li class="bi bi-folder"> <?php echo $paths['WRITEPATH'];?></li>
+	<li class="bi bi-file-code"> .env</li>
+</ul>
+</li>
+
+<li class="bi bi-folder mt-3"> <?php echo $paths['FCPATH'];?>
+	<ul class="list-unstyled ps-3">
+		<li class="bi bi-folder"> public
+			<ul class="list-unstyled ps-3">
+				<li class="bi bi-folder"> ui</li>
+				<li class="bi bi-list"> [site downloads / etc]</li>
+			</ul>
+		</li>
 		<li class="bi bi-file-code"> .htaccess</li>
 		<li class="bi bi-file-code"> .user.ini</li>
 		<li class="bi bi-file-code"> php.ini</li>
 		<li class="bi bi-file-code"> index.php</li>
 		<li class="bi bi-file"> robots.txt</li>
-	</ul></li>
+	</ul>
+</li>
 </ul>
 <?php $this->endSection();
 
 $this->section('content'); ?>
 <h4>Directory structure</h4>
 
-<p>CodeIgniter sits in <mark>~/gymevent.ci4</mark>.</p>
+<p>CodeIgniter sits in <?php echo $paths['ROOTPATH'];?>. This path sits outside the document root.</p>
 
-<p>You will have to edit <mark>~/gymevent.ci4/.env</mark> according to the specific set-up of each server. You will need database connection information and default URL.</p>
+<p><?php echo $paths['SYSTEMPATH'];?> stores the files that make up the framework itself. An upgrade of CodeIgniter system will overwrite this folder. Edit nothing in this folder.</p>
 
-<p><mark>~/gymevent.ci4/system</mark> stores the files that make up the framework itself. An upgrade of CodeIgniter system will overwrite this folder. Edit nothing in this folder.</p>
+<p><?php echo $paths['APPPATH'];?> holds the GymEvent application. Edit files in here to alter the application's behaviour.</p>
 
-<p><mark>~/gymevent.ci4/app</mark> holds the GymEvent application. Edit files in here to alter the application's behaviour.</p>
+<p><?php echo $paths['WRITEPATH'];?> will be used to write cache files and logs / etc. It can be empty for a new installation, but make sure it is writeable.</p>
 
-<p><mark>~/gymevent.ci4/writable</mark> will be used to write cache files and logs / etc. It can be empty for a new installation, but make sure it is writeable.</p>
+<p><?php echo $paths['FCPATH'];?> is the document root (publicly accessible) for this domain. You may have to use a different folder name for this.</p>
 
-<p><mark>~/public_html</mark> is the document root for this domain. You may have to use a different folder name for this.</p>
-
-<p>Ensure <mark>~/public_html/php.ini</mark> and <mark>~/public_html/.user.ini</mark> contain PHP ini files. Ensure they are appropriate for the server.</p>
-
-<p>Edit <mark>~/public_html/index.php</mark> to make <code>$pathsPath</code> point to <mark>~/gymevent.ci4/app/Config/Paths.php</mark>. Example:<br><code>$pathsPath = realpath(FCPATH . '../gymevent.ci4/app/Config/Paths.php');</code></p>
-
-<p><mark>~/public_html/public</mark> is used for downloads and files accessible to the public. All front-end <abbr title="e.g. bootstrap, jQuery, CSS">site resources</abbr> are in <mark>~/public_html/public/ui</mark>.</p>
+<p><?php echo path_label('FCPATH', 'public');?>
+is used for downloads and files accessible to the public. All front-end <abbr title="e.g. bootstrap, jQuery, CSS">site resources</abbr> are in 
+<?php echo path_label('FCPATH', 'public/ui');?>.</p>
 
 <p>Read about CodeIgniter's <a href="https://codeigniter.com/user_guide/concepts/structure.html">directory structure</a>.</p>
+
+<h4>Installation edits</h4>
+
+<p>Edit <?php echo path_label('ROOTPATH', '.env');?> according to the specific set-up of each server. Include database connection information and default URL.</p>
+
+<p>Ensure the php.ini files (<?php echo path_label('FCPATH', 'php.ini');?> &amp; <?php echo path_label('FCPATH', '.user.ini');?> are appropriate for the server.</p>
+
+<p>Edit the front controller (<?php echo path_label('FCPATH', 'index.php');?>) to make <code>$pathsPath</code> point to <?php echo path_label('APPPATH', 'Config/Paths.php');?>.<br>Example: <code>$pathsPath = realpath(FCPATH . '../<em>{ci4}</em>/app/Config/Paths.php');</code></p>
+
 <?php $this->endSection();
 
 $this->section('bottom'); ?>
-<h4>Core constants</h4>
+<h4>Core Constants</h4>
 <ul class="list-unstyled">
-<?php foreach(['APPPATH', 'ROOTPATH', 'SYSTEMPATH', 'FCPATH', 'VIEWPATH', 'PUBLICPATH'] as $const) {
+<?php foreach($consts as $const) {
 	printf('<li>%s: <code>%s</code></li>', $const, constant($const));
 } ?>
 </ul>
+<p><a href="https://codeigniter.com/user_guide/general/common_functions.html#core-constants" title="CodeIgniter help">Read about CodeIgniter constants here</a>.</p>
 
 <h4>Enable Rewrite</h4>
-<p><mark>~/public_html/.htaccess</mark> contains instructions for the  "rewrite engine".</p>
+<p><?php echo path_label('FCPATH', '.htaccess');?> contains instructions for the "rewrite engine" to route requests for non-existent resources to the front controller (<code>/index.php</code>).</p>
 
 <p>Make sure the server domain <em>allows</em> rewrite (it probably doesn't by default). Edit the httpd configuration<br>
 <code>sudo nano /etc/apache2/sites-available/default</code></p>
@@ -77,3 +102,10 @@ $this->section('bottom'); ?>
 sudo service apache2 restart</code></p>
 
 <?php $this->endSection();
+
+function path_label($const_name, $file='') {
+	$title = rtrim(constant($const_name), DIRECTORY_SEPARATOR) . "/{$file}";
+	$text = $const_name;
+	if($file) $text .= "/{$file}";
+	return sprintf('<mark title="%s">%s</mark>', $title, $text);
+}
