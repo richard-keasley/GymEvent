361e215d445a46b7c49ceed803954ee09c5d2921
Richard Keasley <rk@base-camp.org.uk>
2022-03-04 13:57:51 +0000

    Update hardware.php

--- dev.ci4/app/Views/about/hardware.php
+++ dev.ci4/app/Views/about/hardware.php
@@ -4,6 +4,9 @@ $this->section('content');
 ?>
 
 <img src="/app/profile/logo.png" style="width:6em; float:right">
+
+<p>Here is a brief summary of the hardware you will need to run an event.</p>
+
 <h3>Client laptops</h3>
 <p>Client laptops can be almost anything that can run a browser. We have laptops you can use if you need.</p>
 <ul>
@@ -14,9 +17,9 @@ $this->section('content');
 
 <h3>Additionally</h3>
 <ul>
-<li>Ensure you have lots of long ectensin leads to reach to each judge station.</li>
+<li>Ensure you have lots of long extension leads to reach to each judge station.</li>
 <li>You will need a machine from us to play music. Ensure you have a lead to connect the laptop to whatever PA system, you are using.</li>
-<li>You may need a router if using a <abbr title="Local network">LAN</abbr> (rather than direct connection via the internet.</li>
+<li>You may need a router if using a <abbr title="Local network">LAN</abbr> (rather than direct connection via the internet).</li>
 </ul>
 
 <p><img src="/app/profile/image.png"></p>
