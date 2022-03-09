25bc548a5b5cfe585ef90e582a669b6aaa62cdbe
Richard Keasley <rk@base-camp.org.uk>
2022-03-09 13:25:45 +0000

    Update index.php

--- dev.ci4/app/Views/general/admin/index.php
+++ dev.ci4/app/Views/general/admin/index.php
@@ -1,15 +1,15 @@
-<?php $this->extend('default');
- 
-$this->section('content');?>
-<p>Discover more about General Gymnastics in the South-east region.</p>
-<?php $this->endSection(); 
-
-$this->section('sidebar');
-$nav = [
-	['general/intention', 'Intention sheets'],
-	['admin/general/rules/fx', 'Floor rules']
-];
-$navbar = new \App\Views\Htm\Navbar($nav);
-echo $navbar->htm();
-
+<?php $this->extend('default');
+ 
+$this->section('content');?>
+<p>Discover more about General Gymnastics in the South-east region.</p>
+<?php $this->endSection(); 
+
+$this->section('sidebar');
+$nav = [
+	['general/intention', 'Intention sheets'],
+	['admin/general/rules/fx', 'Floor rules']
+];
+$navbar = new \App\Views\Htm\Navbar($nav);
+echo $navbar->htm();
+
 $this->endSection(); 
