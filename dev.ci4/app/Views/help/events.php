e801ed142fac67f63b05096b125c5e76a4e603ce
Richard Keasley <rk@base-camp.org.uk>
2022-03-18 14:28:12 +0000

    tidy up

--- dev.ci4/app/Views/help/events.php
+++
@@ -1,1 +0,0 @@
-<p>You need to be logged in to enter an event, or edit an existing club return.</p>
--- dev.ci4/app/Views/help/index.php
+++
@@ -1,3 +0,0 @@
-<p>Browse the <a href="<?php echo base_url('events');?>">events</a> to find the event you are interested in. From there you will see the services that are currently enabled for that event.</p>
-<hr>
-<p>Please ask Richard if you can't see what you want.</p>
