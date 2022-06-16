b97327db71e72994f494906ef862d33408af9579
richard-keasley <67803679+richard-keasley@users.noreply.github.com>
2022-06-16 09:01:23 +0100

    Update index.php
    hidden link for data download if not logged in

--- dev.ci4/app/Views/general/index.php
+++ dev.ci4/app/Views/general/index.php
@@ -1,20 +1,19 @@
 <?php $this->extend('default');
+$can_edit = \App\Libraries\Auth::check_path('admin/general');
+$app_path = 'app/general';
+
+$files = new \CodeIgniter\Files\FileCollection();
+$files->addDirectory(FCPATH . $app_path);
+$files->removePattern('#logo\.#i');
+if(!$can_edit) $files->removePattern('#\.xls#i');
  
 $this->section('content');
-$app_path = 'app/general';
 ?>
-
 <div class="item-image"><img src="<?php echo base_url("{$app_path}/logo.png");?>"></div>
 <p>Discover more about General Gymnastics in the South-east region.</p>
 <p>Please tell Kim (Brighton) or Dave (Pegasus) if you see a problem on these pages.</p>
 
 <h4>Downloads</h4>
-<?php 
-$files = new \CodeIgniter\Files\FileCollection();
-$files->addDirectory(FCPATH . $app_path);
-$files->removePattern('#logo\.#i');
-?>
-
 <ul class="list-group"><?php 
 $trim_start = strlen(FCPATH);
 foreach($files as $file) {
