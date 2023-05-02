a243eaf1ee4a0777743f6eb6babef83e9939aaec
Richard Keasley <rk@base-camp.org.uk>
2023-05-02 11:02:32 +0100

    Update email.php

--- dev.ci4/app/Views/music/email.php
+++ dev.ci4/app/Views/music/email.php
@@ -1,12 +1,12 @@
 <?php 
 $eventdate = new \CodeIgniter\I18n\Time($event->date);
-$closing = $eventdate->subDays(8);
+$closing = $eventdate->subDays(16);
 $url = base_url("events/view/{$event->id}");
 ?>
 <p>Dear club,</p>
 <p>This is a reminder you must complete your music upload for the 
 <em><?php echo $event->title;?></em>
-before <strong><?php echo $closing->toLocalizedString('eeee d MMMM Y');?></strong> when the service will close.</p>
+before <strong>8:00pm on <?php echo $closing->toLocalizedString('eeee d MMMM Y');?></strong> when the service will close.</p>
 <p>Please upload your remaining music as soon as you can.</p>
 <p><?php echo anchor($url);?></p>
 <p>Please contact Richard if you are struggling to do this.</p>
