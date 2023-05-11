50b2538be898b550668c5f18abb56265b80854c6
Richard Keasley <rk@base-camp.org.uk>
2023-05-11 12:57:09 +0100

    Add new user
    bug: Can't add user without abbr. abbr now sanitizes input rather than validates.

--- dev.ci4/app/Models/Users.php
+++ dev.ci4/app/Models/Users.php
@@ -14,7 +14,6 @@ protected $beforeInsert = ['clean_save'];
 
 protected $validationRules = [
 	'name'  => 'required|min_length[5]|alpha_numeric_punct|is_unique[users.name,id,{id}]',
-	'abbr' => 'max_length[5]|alpha_numeric_punct',
 	'email' => 'valid_email|is_unique[users.email,id,{id}]',
 	'password' => 'required|min_length[6]'
 ];
@@ -23,9 +22,6 @@ protected $validationMessages = [
 	'name' => [
 		'is_unique' => 'That user name has already been used.',
 		'alpha_numeric_punct' => 'The user name should only contain letters and numbers'
-	],
-	'abbr' => [
-		'alpha_numeric_punct' => 'The short name (abbr) contains invalid characters'
 	],
 	'email' => [
 		'valid_email' => 'Please enter a valid email address',
@@ -44,6 +40,10 @@ protected function clean_save($arr) {
 	}
 	if(isset($arr['data']['password'])) {
 		$arr['data']['password'] = password_hash($arr['data']['password'], PASSWORD_DEFAULT);
+	}
+	if(isset($arr['data']['abbr'])) {
+		$arr['data']['abbr'] = preg_replace('/[^ \w\-\&]/', '', $arr['data']['abbr']);
+		# d($arr['data']['abbr']);
 	}
 	return $arr;
 }
