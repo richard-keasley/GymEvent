85672070356c43a21e3e8a7d1be303ab3884e672
Richard Keasley <rk@base-camp.org.uk>
2023-09-04 10:41:34 +0100

    Update follow.php

--- dev.ci4/app/Views/scoreboard/follow.php
+++ dev.ci4/app/Views/scoreboard/follow.php
@@ -19,4 +19,5 @@ $this->section('content'); ?>
 <p class="display-3">Scan to follow the scores for today's event!</p>
 <p class="display-5">You will need the entry number of the gymnast you are following.</p>
 <p class="display-6"><?php echo anchor($link, $link, $anchor);?></p>
+<img src="/app/profile/logo.png" alt="GymEvent" style="width:3cm; position:fixed;right:0; border:0; bottom:0;">
 <?php $this->endSection();
