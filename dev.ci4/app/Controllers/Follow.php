037aa44010d6e37673ce968b850b18f2807c5db9
Richard Keasley <67803679+richard-keasley@users.noreply.github.com>
2023-11-22 23:49:40 +0000

    link to follow scores

--- dev.ci4/app/Controllers/Follow.php
+++ dev.ci4/app/Controllers/Follow.php
@@ -1,10 +1,11 @@
 <?php namespace App\Controllers;
 
-class Follow  extends \App\Controllers\BaseController {
+class Follow extends \App\Controllers\BaseController {
 
 public function index() {
-	$follow = 'https://judging.k78design.co.uk/kiosk#/entry-info';
-	return redirect()->to($follow);
+	// ToDo: make this editable from UI
+	$link = 'https://judging.k78design.co.uk/kiosk#/entry-info';
+	return redirect()->to($link);
 }
 
 }
--- dev.ci4/app/Views/scoreboard/follow.php
+++ dev.ci4/app/Views/scoreboard/follow.php
@@ -1,15 +1,16 @@
 <?php $this->extend('default');
 helper('html');
-$link = 'https://sb.gymevent.uk/kiosk#/entry-info';
+
+$link = 'follow';
 
 $img = [
-    'src'   => '/app/scoreboard/follow.svg',
+    'src'   => '/app/scoreboard/qr-follow.png',
     'alt'   => 'Follow scores',
     'style' => "width:12cm;max-width:100%;",
 ];
 
 $anchor = [
-	'title' => $link,
+	'title' => base_url($link),
 	'class' => "d-block"
 ];
 
@@ -18,6 +19,6 @@ $this->section('content'); ?>
 <div class="text-center"><?php echo anchor($link, img($img), $anchor);?></div>
 <p class="display-3">Scan to follow the scores for today's event!</p>
 <p class="display-5">You will need the entry number of the gymnast you are following.</p>
-<p class="display-6"><?php echo anchor($link, $link, $anchor);?></p>
+<p class="display-6"><?php echo anchor($link, base_url($link), $anchor);?></p>
 <img src="/app/profile/logo.png" alt="GymEvent" style="width:3cm; position:fixed;right:0; border:0; bottom:0;">
 <?php $this->endSection();
