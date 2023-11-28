9b90111aa0e4cbeca32fd67214227f0ca5c51211
Richard Keasley <67803679+richard-keasley@users.noreply.github.com>
2023-11-28 09:24:38 +0000

    Update Playtrack.php

--- dev.ci4/app/Views/Htm/Playtrack.php
+++ dev.ci4/app/Views/Htm/Playtrack.php
@@ -3,40 +3,53 @@
 <p class=""></p>
 <script>
 const playtrack = {
-	load: function(track_url, autoplay=1) {
-		playtrack.pause();
-		if(track_url) {
-			var temp = track_url.split('/').pop(); // filename
-			var html = temp.split('?')[0]; // remove query
-			html = html.replace(/^0+/, ''); // trim leading zeros
-			html = html.replace('.', ' (') + ')'; // separate extension into bracket
-			html = html.replace('_', ' ');
-			playtrack.msg(html, 'success');
-			playtrack.player.attr('src', track_url);
-			// attempt to preload music
-			// tmp = jQuery.get(track_url);
-			// playtrack.player.trigger('load');
-			if(autoplay) {
-				playtrack.player.trigger('play');
-			}
+load: function(track_url, autoplay=1) {
+	playtrack.pause();
+	if(track_url) {
+		var temp = track_url.split('/').pop(); // filename
+		var html = temp.split('?')[0]; // remove query
+		html = html.replace(/^0+/, ''); // trim leading zeros
+		html = html.replace('.', ' (') + ')'; // place extension in bracket
+		html = html.replace('_', ' ');
+		playtrack.msg(html, 'warning');
+		playtrack.audio.src = track_url;
+		playtrack.audio.muted = false;
+		playtrack.audio.volume = 1;
+		
+		playtrack.autoplay = autoplay;
+		
+		// attempt to preload music
+		// playtrack.audio.muted = true;
+		// playtrack.audio.play();
+		// tmp = jQuery.get(track_url);
+		// playtrack.audio.trigger('play');
+		// playtrack.audio.trigger('pause');
+		
+		if(autoplay) {
+			// playtrack.audio.play();
 		}
-	},
-	pause: function() {
-		playtrack.msg('ready&hellip;', 'light');
-		playtrack.player.trigger('pause');
-	},
-	msg: function(html, alert) {
-		playtrack.message.innerHTML = html;
-		playtrack.message.className = 'p-1 my-0 alert alert-' + alert;
-	},
-	player: $('#playtrack audio'),
-	message: $('#playtrack p')[0]
+	}
+},
+
+pause: function() {
+	playtrack.msg('ready&hellip;', 'light');
+	playtrack.audio.pause();
+},
+
+msg: function(html, alert) {
+	playtrack.message.innerHTML = html;
+	playtrack.message.className = 'p-1 my-0 alert alert-' + alert;
+},
+
+audio: $('#playtrack audio')[0],
+message: $('#playtrack p')[0],
+autoplay: 1
 };
 
 $(function() {
 playtrack.pause();
 
-playtrack.player.on("error", function(e) {
+playtrack.audio.onerror = (event) => {
 	var html;
 	switch(e.target.error.code) {
 		case e.target.error.MEDIA_ERR_ABORTED: html = 'Download aborted'; break;
@@ -45,9 +58,15 @@ playtrack.player.on("error", function(e) {
 		case e.target.error.MEDIA_ERR_SRC_NOT_SUPPORTED: html = 'No decoder available'; break;
 		default: html = 'Unknown error';
 	}
-	html = '<a href="' + e.target.src + '" title="try to download this track" target="music">'+html+'</a>';
+	html = '<a href="' + e.target.src + '" title="try to download this track" target="music">' + html + '</a>';
 	playtrack.msg(html, 'danger');
-});
+};
+
+playtrack.audio.oncanplaythrough = (event) => {
+	var html = playtrack.message.innerHTML;
+	playtrack.msg(html, 'success');
+	if(playtrack.autoplay) playtrack.audio.play();
+};
 	
 });
 </script>
