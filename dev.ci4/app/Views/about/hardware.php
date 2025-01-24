0697714599e9b26940e3bb11d56e92e3bb1d86ca
Richard Keasley <rk@base-camp.org.uk>
2025-01-24 09:31:02 +0000

    Update hardware.php

--- dev.ci4/app/Views/about/hardware.php
+++ dev.ci4/app/Views/about/hardware.php
@@ -4,18 +4,16 @@ $this->section('content'); ?>
 
 <p>Here is a summary of the hardware you may need to run an event.</p>
 <p>We have some reconditioned laptops and old TVs if you need them.</p>
-
+<p>Yuu will access to the internet (WifI / etc) at the venue.</p>
 <h3>Client laptops</h3>
-<p>Client laptops can be almost anything that can run a browser. Any device will do, and we encourage users to provide their own device (be it laptop, tablet, mobile phone).</p>
+<p>Client laptops can be almost anything that can run a browser (e.g. Chrome, Edge, Firefox, Safari). Any device will do, and we encourage users to provide their own device (e.g. laptop, tablet, mobile phone).</p>
 <ul>
 <li>1 for each judging panel</li>
 <li>1 for registration desk</li>
 <li>1 for announcer</li>
 </ul>
-
 <h3>Score display</h3>
 <p>Each display needs a laptop and a (large) TV.</p>
-
 <h3>Additionally</h3>
 <ul>
 <li>Ensure you have lots of long extension leads to reach to each judge station.</li>
